0a89c5cca8ab5c644e59735f7d84e5dbebc7154b
R-oxy <aronmangati@gmail.com>
2024-06-11 00:25:50 -0700

    task 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Async generator """
+""" Async Comprehensions """
 import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
